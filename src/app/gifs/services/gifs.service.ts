6eb7c24dd5d5d48bdd8611a2ed22d73f49b45da1
Gabriel Peña <penagabriel22@gmail.com>
2024-08-11 00:46:58 -0400

    fix api key

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,7 +8,7 @@ export class GifsService {
   public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
-  private apiKey:       string = 'dGMJW12nlHSz46O3sy81BZFryoPshmUk';
+  private apiKey:       string = 'REoJvy2Ie22gadwNjw5pizphMGNbmPbM';
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
   constructor( private http: HttpClient ) {
